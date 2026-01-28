c547fee6eb8d097027168626c89d98d076b178f8
Timofey2339 <kirpatimofey@gmail.com>
2026-01-29 00:04:08 +0200

    Fix URL case sensitivity for Crucifix loading

--- Entity Spawner/Crucfix.lua
+++ Entity Spawner/Crucfix.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://github.com/Timofey2339/Custom-Entities-For-spawner/main.Functions.lua"))()
-LoadCustomInstance("https://github.com/Timofey2339/Custom-Entities-For-spawner/blob/main/Item%20spawner/Crucifix.rbxm").Parent = game:GetService("Players").LocalPlayer.Backpack
+LoadCustomInstance("https://github.com/Timofey2339/Custom-Entities-For-spawner/blob/main/Item%20Spawner/Crucifix.rbxm").Parent = game:GetService("Players").LocalPlayer.Backpack
